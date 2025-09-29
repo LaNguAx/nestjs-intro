b99082d406fed9d76a25f1b36a60e52d065c53f8
LaNguAx <itayakni@gmail.com>
2025-09-29 19:08:21 +0300

    Update dependencies and enhance user management: added DTOs for user creation and patching, implemented validation pipes, and refactored user controller methods for improved parameter handling.

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,37 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsEmail,
+  MinLength,
+  MaxLength,
+  Matches,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(96)
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(2)
+  @MaxLength(96)
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(256)
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/, {
+    message:
+      'Minimum 8 characters password, it must contain at least one letter, one number and one special character',
+  })
+  password: string;
+}
